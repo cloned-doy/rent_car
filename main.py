cdee893cb953b646854d84311690827adcf82eef
doy <doy@doy.doy>
2023-09-07 16:29:44 +0700

    init makeup

--- main.py
+++ main.py
@@ -4,14 +4,27 @@ learn CRUD for killing time
 
 print("YoCar")
 
-class Database():
-	pass
+class CarGarage:
 
-def add_car():
-	pass
+	""" the greatest fictional garace for beatiful cars ever existed in the world. """
 
-def search_car():
-	pass
+	def __init__(self, car=None, path="car.db"):
+		self.car = car
+		self.path = path
 
-def rent_car():
-	pass
+    # def database(self):
+    	# self.db = [['index', 'car', 'year', 'rented'],
+			  #   	[0, 'model x', 2020, True],
+			  #   	[1, 'semitruck', 1998, False],
+			  #   	[2, 'roadster', 2023, False],
+			  #   	]
+		# pass
+
+	def add_car(self):
+		pass
+
+	def search_car(self):
+		pass
+
+	def rent_car(self):
+		pass
